572ca1281d20134be48349dac0dedeb1b51786a5
Alexis Beauregard <mariposa_lex99@yahoo.com>
2023-06-04 18:27:30 -0400

    Completes App state and events

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,14 @@
-import React from "react";
+/** @format */
+
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("");
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +16,11 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {items
+          .filter((item) => selectedCategory === "" || item.category === selectedCategory)
+          .map((item) => (
+            <Item key={item.id} name={item.name} category={item.category} />
+          ))}
       </ul>
     </div>
   );
